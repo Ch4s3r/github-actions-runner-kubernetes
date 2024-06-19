c0f7a11e702224826142fbc7906dda4b1b05bd26
Ch4s3r <lamprecht.patrick1@gmail.com>
2024-06-19 18:05:30 +0200

    chore: Update run.sh to use github-token secret for GitHub Actions runner configuration

--- run.sh
+++ run.sh
@@ -3,18 +3,18 @@
 
 kubectl create ns arc-systems
 kubectl create ns arc-runners
-kubectl create secret generic pre-defined-secret \
+kubectl create secret generic github-token \
    --namespace=arc-runners \
    --from-literal=github_token=$GITHUB_TOKEN
 
 helm install arc \
     --namespace arc-systems \
-    --set githubConfigSecret=pre-defined-secret \
+    --set githubConfigSecret=github-token \
     oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set-controller
 
 helm install arc-runner-set \
     --namespace arc-runners \
     --create-namespace \
     --set githubConfigUrl=https://github.com/Ch4s3r/github-actions-runner-kubernetes \
-    --set githubConfigSecret=pre-defined-secret \
+    --set githubConfigSecret=github-token \
     oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set
