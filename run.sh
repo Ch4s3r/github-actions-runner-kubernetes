aa25591e1a37fe4f1e83c8247d344dd2d7a55fc8
Ch4s3r <lamprecht.patrick1@gmail.com>
2024-06-19 17:46:01 +0200

    feat: Add Kubernetes runner configuration files
    This commit adds the necessary configuration files for setting up a Kubernetes runner in the repository. The `kubernetes-runner.yaml` file defines a GitHub Actions workflow that runs on a runner scale set. The `run.sh` script sets up the Kubernetes cluster, creates the required namespaces, and installs the Actions Runner Controller and the runner scale set.

---
+++ run.sh
@@ -0,0 +1,21 @@
+# export GITHUB_TOKEN=...
+
+k3d create cluster
+
+kubectl create ns arc-systems
+kubectl create ns arc-runners
+kubectl create secret generic pre-defined-secret \
+   --namespace=arc-runners \
+   --from-literal=github_token=$GITHUB_TOKEN
+
+helm install arc \
+    --namespace arc-systems \
+    --set githubConfigSecret=pre-defined-secret \
+    oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set-controller
+
+helm install arc-runner-set \
+    --namespace arc-runners \
+    --create-namespace \
+    --set githubConfigUrl=https://github.com/Ch4s3r/github-actions-runner-kubernetes \
+    --set githubConfigSecret=pre-defined-secret \
+    oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set
