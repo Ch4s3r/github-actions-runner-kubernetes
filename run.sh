9e177d8aafbd36a0e2764220bf99af8117aa0366
Ch4s3r <lamprecht.patrick1@gmail.com>
2025-04-10 22:03:33 +0200

    chore: Update run.sh to handle namespace creation and add volume claim settings

--- run.sh
+++ run.sh
@@ -7,21 +7,24 @@ export GITHUB_PAT=$GITHUB_COM_TOKEN
 # export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
 NAMESPACE="arc-systems"
-kubectl create ns "${NAMESPACE}"
+kubectl create ns "${NAMESPACE}" || true
 helm upgrade -i arc \
     --namespace "${NAMESPACE}" \
     oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set-controller
 INSTALLATION_NAME="arc-runner-set"
 NAMESPACE="arc-runners"
-kubectl create ns "${NAMESPACE}"
+kubectl create ns "${NAMESPACE}" || true
 kubectl create secret generic github-credentials \
   --namespace "${NAMESPACE}" \
-  --from-literal=github_token="${GITHUB_PAT}"
+  --from-literal=github_token="${GITHUB_PAT}" || true
 helm upgrade -i "${INSTALLATION_NAME}" \
     --namespace "${NAMESPACE}" \
     --set githubConfigUrl="${GITHUB_CONFIG_URL}" \
     --set githubConfigSecret=github-credentials \
     --set containerMode.type=kubernetes \
+    --set containerMode.kubernetesModeWorkVolumeClaim.accessModes[0]=ReadWriteOnce \
+    --set containerMode.kubernetesModeWorkVolumeClaim.storageClassName=local-path \
+    --set containerMode.kubernetesModeWorkVolumeClaim.resources.requests.storage=1Gi \
     --set minRunners=1 \
     oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set
 #    --set runnerGroup=ghe-local-arc \
